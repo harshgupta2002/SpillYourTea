7239d5fba32c77d23fd421d5c74e7148abe94930
Mayank Singh <99077491+mayankthechamp@users.noreply.github.com>
2024-07-25 22:24:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home";
 import SideMenu from "./Components/SideMenu/SideMenu";
-import Card from './Components/Card';
-import profilePicture from '../src/components/House 2.png';
+// import Card from './Components/Card';
+// import profilePicture from '../src/components/House 2.png';
 
 function App() {
   return (
@@ -12,45 +12,6 @@ function App() {
       <div className="flex justify-between">
         <SideMenu/>
         <Home/>
-    <Card
-        title="Exciting Gossip!"
-        snippet="Did you hear about the latest drama?"
-        author="John Doe"
-        likes={120}
-        comments={34}
-        timestamp="2 hours ago"
-        profilePicture={profilePicture}
-      />
-      <Card
-        title="Exciting Gossip!"
-        snippet="Did you hear about the latest drama?"
-        author="John Doe"
-        likes={120}
-        comments={34}
-        timestamp="2 hours ago"
-        profilePicture={profilePicture}
-      />
-
-       <Card
-        title="Exciting Gossip!"
-        snippet="Did you hear about the latest drama?"
-        author="John Doe"
-        likes={120}
-        comments={34}
-        timestamp="2 hours ago"
-        profilePicture={profilePicture}
-      />
-
-        <Card
-        title="Exciting Gossip!"
-        snippet="Did you hear about the latest drama?"
-        author="John Doe"
-        likes={120}
-        comments={34}
-        timestamp="2 hours ago"
-        profilePicture={profilePicture}
-      />
-      
       </div>
     </>
   );
