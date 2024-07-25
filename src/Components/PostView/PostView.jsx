998b3a81516008c810f6017925548fc8c55b72f1
harshgupta2002 <gup.harsh.2002@gmail.com>
2024-07-25 22:09:55 +0530

    Updated home

--- src/Components/PostView/PostView.jsx
+++ src/Components/PostView/PostView.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 function PostView() {
   return (
     <div>
-        <div className='fixed h-full w-full bg-black bg-opacity-90'>
-            <div className=' bg-[#1E1E1D] h-[70%] w-[70%] shadow-xl shadow-black rounded-xl'>
+        <div className='fixed z-[999] top-0 left-0 h-full flex justify-center items-center w-full bg-black bg-opacity-90'>
+            <div className=' flex justify-center bg-[#1E1E1D] h-[80%] w-[70%] shadow-xl shadow-black rounded-xl'>
+                <div className=' '>
 
+                </div>
+                
+                <div></div>
             </div>
         </div>
     </div>
