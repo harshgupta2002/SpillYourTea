01f42dfc759faa9beac7042a8aca0c3156e39a79
Mayank Singh <99077491+mayankthechamp@users.noreply.github.com>
2024-07-25 20:43:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import profilePicture from '../src/components/House 2.png';
+import Card from './components/Card';
 
 function App() {
   return (
     <>
       <Header/>
       <Home/>
+    <Card
+        title="Exciting Gossip!"
+        snippet="Did you hear about the latest drama?"
+        author="John Doe"
+        likes={120}
+        comments={34}
+        timestamp="2 hours ago"
+        profilePicture={profilePicture}
+      />
+      <Card
+        title="Exciting Gossip!"
+        snippet="Did you hear about the latest drama?"
+        author="John Doe"
+        likes={120}
+        comments={34}
+        timestamp="2 hours ago"
+        profilePicture={profilePicture}
+      />
+
+       <Card
+        title="Exciting Gossip!"
+        snippet="Did you hear about the latest drama?"
+        author="John Doe"
+        likes={120}
+        comments={34}
+        timestamp="2 hours ago"
+        profilePicture={profilePicture}
+      />
+
+        <Card
+        title="Exciting Gossip!"
+        snippet="Did you hear about the latest drama?"
+        author="John Doe"
+        likes={120}
+        comments={34}
+        timestamp="2 hours ago"
+        profilePicture={profilePicture}
+      />
+      
     </>
   );
 }
